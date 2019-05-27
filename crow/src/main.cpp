35ca6ab10b66019346deaf6b44cbceaf8e4fccff
Bruno Vieira <brunooliveiravieira@gmail.com>
2019-05-26 21:51:00 -0300

    Implement return of json for crow

--- crow/src/main.cpp
+++ crow/src/main.cpp
@@ -4,8 +4,20 @@ using namespace std;
 int main(int argc, char ** argv) {
     crow::SimpleApp app;
 
-    CROW_ROUTE(app, "/")([]() {
-        return "Hello, World!";
+    CROW_ROUTE(app, "/")([](const crow::request& re) {
+        vector<crow::json::wvalue> items;
+        char _id[20];
+        for (int i = 0; i < 10000; ++i) {
+            snprintf(_id, size_t(_id), "item-%d", i);
+            crow::json::wvalue item;
+            item["id"] = _id;
+            item["name"] = "Hello, world!";
+            item["type"] = "application";
+            items.emplace_back(std::move(item));
+        }
+        crow::json::wvalue result;
+        result = std::move(items);
+        return crow::response{result};
     });
 
     char * port = getenv("PORT");
