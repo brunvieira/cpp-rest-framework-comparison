3eb74db4320708f7b5af4d4d93975f9263da2e73
Bruno Vieira <brunooliveiravieira@gmail.com>
2019-06-11 11:42:11 -0300

    Implement size path parameter on restbed

--- restbed/src/main.cpp
+++ restbed/src/main.cpp
@@ -7,27 +7,35 @@
 using namespace std;
 using namespace restbed;
 
-void get_method_handler( const shared_ptr< Session > session )
+void get_method_handler(const shared_ptr<Session> session)
 {
-    const auto request = session->get_request( );
-
-    int content_length = request->get_header( "Content-Length", 0 );
-
-    session->fetch( content_length, [ ]( const shared_ptr< Session > session, const Bytes & body )
-    {
-        string json = make_json_array(10000);
-        session->close( OK, json, { { "Content-Length", to_string(json.length()) } } );
-    } );
+    const auto& request = session->get_request();
+    const string sizeParameter = request->get_path_parameter("size");
+    int size = 0;
+    try {
+        size = stoi(sizeParameter);
+    } catch (...) {
+        session->close(restbed::BAD_REQUEST);
+    }
+    if (!size) {
+        return;
+    }
+    string json = make_json_array(size);
+    std::multimap<std::string, std::string> headers = {
+        {"Content-Length", to_string(json.length())},
+        {"Content-Type", "application/json"}
+    };
+    session->close(OK, json, headers);
 }
 
 int main( const int, const char** )
 {
     uint16_t port = server_port(18005);
-    auto resource = make_shared< Resource >( );
-    resource->set_path( "/" );
-    resource->set_method_handler("GET", get_method_handler );
+    auto resource = make_shared<Resource>();
+    resource->set_path("/json/array/{size: \\d*}");
+    resource->set_method_handler("GET", get_method_handler);
 
-    auto settings = make_shared< Settings >( );
+    auto settings = make_shared< Settings >();
     settings->set_port(port);
     settings->set_default_header("Connection", "close");
 
